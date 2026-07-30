393e5d1e079a3a1469deaf96c13cfed3d759680b
Saksham Jonchhe <sakshamjonchhe@gmail.com>
2026-07-30 22:30:51 +0545

    new

---
+++ Basics/forloops.py
@@ -0,0 +1,4 @@
+#for loops
+
+for x in range(21,1,-1):
+    print(x)
---
+++ Basics/nestedloops.py
@@ -0,0 +1,14 @@
+#nested loops 
+
+rows=int(input("Enter the number of rows:"))
+columns=int(input("Enter the number of columns:"))
+symbol=input("Enter the symbols:")
+
+
+
+for x in range(rows):
+    for  y in range(columns):
+        print(symbol,end="")
+    print()    
+
+    
