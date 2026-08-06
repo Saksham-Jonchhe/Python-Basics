e2e532b5fd252564a9afb17a137012dcf3272480
Saksham Jonchhe <sakshamjonchhe@gmail.com>
2026-08-06 14:17:52 +0545

    Basics

--- Basics/rps4.py
+++ Basics/rps4.py
@@ -1,6 +1,7 @@
 import sys
 import random
 from enum import Enum
+game_count=0
 
 def play_rps():
 
@@ -35,19 +36,25 @@ def play_rps():
     print("Python chose " + computerchoice + ".")
     print("")
 
-    if player == 1 and computer == 3:
-        print(" You win!")
-    elif player == 2 and computer == 1:
-        print(" You win!")
-    elif player == 3 and computer == 2:
-        print(" You win!")
-    elif player == computer:
-        print(" Tie game!")
-    else:
-        print(" Python wins!")
+    def decide_winner(player,computer):
+        if player == 1 and computer == 3:
+            return" You win!"
+        elif player == 2 and computer == 1:
+            return" You win!"
+        elif player == 3 and computer == 2:
+            return" You win!"
+        elif player == computer:
+            return" Tie game!"
+        else:
+            return" Python wins!"
 
-
+    game_result= decide_winner(player,computer)
+    print(game_result)
     
+
+    global game_count
+    game_count+=1
+    print("\n Game count: " + str(game_count))
 
     while True:
         
