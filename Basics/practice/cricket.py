4e1a28909b4d1052a8fcdb10199c21cd69c34f76
Saksham Jonchhe <sakshamjonchhe@gmail.com>
2026-08-22 23:24:14 +0545

    Basics

--- Basics/practice/cricket.py
+++ Basics/practice/cricket.py
@@ -21,19 +21,19 @@ class Teams(Player):
         print(f"Team Name:{self.teamname}")
         print(f"PLayers: ")
         for player in self.players:
-            print(f"{player.playername} - {player.playerrole} -{player.playerage}")
+            print(f"{player.playername} - {player.playerrole} - Age:{player.playerage}")
 
 class Match:
     def __init__(self,*teamname):
         self.matches=[]
-        self.teams=teamname
+        self.teams=teamname 
 
     def schedule(self):
         
         self.matches=self.teams
 
     def showmatches(self):
-        print(f"{self.matches[0].teamname}vs{self.matches[1].teamname} ")
+        print(f"{self.matches[0].teamname} vs {self.matches[1].teamname} ")
         # print(f"{self.matches[0].teamname}vs{self.matches[1].teamname} ")
         # print(f"{self.matches[0].teamname}vs{self.matches[1].teamname} ")
         print()
