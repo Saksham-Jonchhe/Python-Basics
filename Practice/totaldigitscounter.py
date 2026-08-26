0d3614b63dddce2fd56a1818ffec24d398a072f6
Saksham Jonchhe <sakshamjonchhe@gmail.com>
2026-08-27 00:16:04 +0545

    Basics

---
+++ Practice/totaldigitscounter.py
@@ -0,0 +1,10 @@
+# Given Input: 75869
+#Use while loop
+# Expected Output: Total digits are: 5
+
+numbers=int(input("Enter a number:"))
+count=0
+while numbers!=0:
+        numbers=numbers//10
+        count+=1
+print(count)    
